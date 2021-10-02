bc4e6f7e0ce71aa6459dbff706724894233ddb42
Jack Sabine <jack@sabine.ws>
2021-10-02 10:21:19 -0400

    push stupid changes

---
+++ synopsys_proj/syn/IncC.tcl
@@ -0,0 +1,7 @@
+if { $argc != 2 }
+    puts "Need two args"
+} else {
+    set CLK_PER = [lindex $argv 1]
+    puts $CLK_PER
+    create_clock -name $clkname -period $CLK_PER -waveform "0 [expr $CLK_PER / 2]" $clkname
+}
